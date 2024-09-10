cfcbf8abfd47b5e6cf7133c87f9e045030b0b3ad
gewentz <gabrielfwtz@gmail.com>
2024-09-10 19:45:07 -0300

    series daiane

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <main className="bg-slate-600">
+      <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️</h1>
+
+      <div>
+        <iframe width="100%" height="600" src="https://painelk7.online/embed/tellmelies/"></iframe>
+      </div>
+    </main>
+  )
+}
+
+export default App
