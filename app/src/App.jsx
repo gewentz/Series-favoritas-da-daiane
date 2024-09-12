b117a8c30836d20fc7f72a6dedb4bda56e45b1e6
gewentz <gabrielfwtz@gmail.com>
2024-09-12 20:53:53 -0300

    "Updated App.jsx: changed h1 text, added iframe, and removed old iframe"

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,10 +2,13 @@ function App() {
 
   return (
     <main className="bg-slate-600">
-      <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️</h1>
+      <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️ 2º TEMPORADA!</h1>
 
       <div>
-        <iframe width="100%" height="600" src="https://painelk7.online/embed/tellmelies/"></iframe>
+      <iframe src="https://vidsrc.me/embed/tv?tmdb=130464" style="width: 100%; height: 100%;" allowfullscreen></iframe>
+      </div>
+
+      <div>
       </div>
     </main>
   )
