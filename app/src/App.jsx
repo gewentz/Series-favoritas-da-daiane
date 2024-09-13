40855f8c53143a25eec0aa1d7147fee6ca1bfa17
gewentz <gabrielfwtz@gmail.com>
2024-09-12 21:00:59 -0300

    update

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,11 @@ function App() {
 
   return (
     <main className="bg-slate-600">
-      <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️ 2º TEMPORADA!</h1>
+      <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️</h1>
 
-      <div>
-      <iframe src="https://vidsrc.me/embed/tv?tmdb=130464" style="width: 100%; height: 100%;" allowfullscreen></iframe>
-      </div>
-
-      <div>
+      <div className="h-svh flex flex-col items-center">
+        <h1 className="text-gray-50 font-black w-full text-center text-xl p-10"> Temporada 2</h1>
+        <iframe src="https://vidsrc.me/embed/tv?imdb=tt13018148&ds_lang=pt" width= "100%" height= "100%;" allowfullscreen></iframe>
       </div>
     </main>
   )
