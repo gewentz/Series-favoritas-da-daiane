0960e711bb21666479f233956e821e3ded7c677d
gewentz <gabrielfwtz@gmail.com>
2024-09-12 22:35:11 -0300

    Remove unnecessary h1 element from App component.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
       <h1 className="text-gray-50 font-black w-full text-center text-xl p-10">Series favoritas do meu amor ❤️</h1>
 
       <div className="h-svh flex flex-col items-center">
-        <h1 className="text-gray-50 font-black w-full text-center text-xl p-10"> Temporada 2</h1>
         <iframe src="https://vidsrc.me/embed/tv?imdb=tt13018148&ds_lang=pt" width= "100%" height= "100%;" allowfullscreen></iframe>
       </div>
     </main>
